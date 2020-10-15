967309d77435eada4fc79bac6f0217e5c813701d
sanderson.ad@googlemail.com <5hp31d26>
2020-10-15 19:15:41 +0100

    CSV directory ordering.

--- orders/load_csv.go
+++ orders/load_csv.go
@@ -12,7 +12,7 @@ func AddCSVOrderFromDir(ot *OrderTable, fpath string, delete bool) {
 	// open the file
 	csvFile, err := os.Open(fpath)
 	if err != nil {
-		log.Fatalf("couldn't open the CSV file: %s", err)
+		log.Fatalf("couldn't open %s, error was %s", fpath, err)
 	}
 	// parse the file
 	r := csv.NewReader(csvFile)
@@ -22,7 +22,28 @@ func AddCSVOrderFromDir(ot *OrderTable, fpath string, delete bool) {
 		log.Fatal(err)
 	}
 	// create orders
-	_ := newOrderFromCSVRow(row)
+	order := newOrderFromCSVRow(row)
+	// check for bid
+	bid, ok := order.(Bid)
+	if ok {
+		log.Printf("added bid to table: (%s, %s, %s, %s)", row[0], row[1], row[2], row[3])
+		ot.Bids = append(ot.Bids, bid) // [TODO]: index into order table, rather than sorting
+	}
+	// check for offer
+	offer, ok := order.(Offer)
+	if ok {
+		log.Printf("added offer to table: (%s, %s, %s, %s)", row[0], row[1], row[2], row[3])
+		ot.Offers = append(ot.Offers, offer) // [TODO]: index into order table, rather than sorting
+	}
+	// close and delete order file, if required
+	csvFile.Close()
+	if delete {
+		log.Print("deleting order at ", fpath)
+		err := os.Remove(fpath)
+		if err != nil {
+			log.Fatalf("couldn't delete order at %s, error was %s", fpath, err)
+		}
+	}
 }
 
 // newOrderFromCSVRow : convert CSV row into an order instance
