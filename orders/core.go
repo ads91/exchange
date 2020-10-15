98db46fba990f1cb36c8a5bc50b72a73d1cf4a82
sanderson.ad@googlemail.com <5hp31d26>
2020-10-15 18:51:05 +0100

    listening for orders in a directory local to the architecture.

--- orders/core.go
+++ orders/core.go
@@ -1,11 +1,25 @@
 package orders
 
 import (
+	"io/ioutil"
+	"log"
 	"sync"
 	"time"
 )
 
-func ScanDir(dir string) []string {}
+func ScanDir(dir string) []string {
+	var orders []string
+	files, err := ioutil.ReadDir(dir)
+	// check for errors
+	if err != nil {
+		log.Fatal(err)
+	}
+	// loop the files
+	for _, file := range files {
+		orders = append(orders, dir+file.Name())
+	}
+	return orders
+}
 
 // ListToDir : listen to a local directory for orders
 func ListenToDir(ot *OrderTable, wg *sync.WaitGroup, f func(ot *OrderTable, fpath string, delete bool), dir string, waitTimeSecs int, delete bool) {
