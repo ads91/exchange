64869231f982d6681df2ddac10cee15468f949d8
sanderson.ad@googlemail.com <5hp31d26>
2020-10-15 18:49:26 +0100

    listening for orders in a directory local to the architecture.

--- orders/core.go
+++ orders/core.go
@@ -1,4 +1,22 @@
 package orders
 
+import (
+	"sync"
+	"time"
+)
+
+func ScanDir(dir string) []string {}
+
 // ListToDir : listen to a local directory for orders
-func ListenToDir()
+func ListenToDir(ot *OrderTable, wg *sync.WaitGroup, f func(ot *OrderTable, fpath string, delete bool), dir string, waitTimeSecs int, delete bool) {
+	var fpaths []string
+	defer wg.Done()
+	// run indefinitely
+	for {
+		// wait
+		time.Sleep(time.Duration(waitTimeSecs) * time.Second)
+		// scan the dir for files
+		fpaths = ScanDir(dir)
+
+	}
+}
